fc15314672a279d81c9ebd3f982d65fa001d3c6f
github username <github email address>
2018-10-01 14:10:01 -0400

    added date validation to Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,14 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :hospital
 
+  validate :date_cannot_be_in_the_future
+
+  def date_cannot_be_in_the_future
+    if date > Date.today
+      errors.add(:date, "Date can't be in the future")
+    end
+  end
+
   def user_name
     self.user.name
   end
