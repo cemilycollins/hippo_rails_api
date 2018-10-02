b5b34e229c965cb68346d65540ba56c79654e49c
github username <github email address>
2018-10-02 13:18:06 -0400

    updated procedures index route

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -15,6 +15,6 @@ class ProceduresController < ApplicationController
   end
 
   def index
-    render json: Procedure.find(params[:id]).to_json(only: [:id, :nat_avg_cost, :name, :procedure_number_string, :total_hospitals, :total_discharges])
+    render json: Procedure.all.to_json(only: [:id, :nat_avg_cost, :name, :procedure_number_string, :total_hospitals, :total_discharges])
   end
 end
