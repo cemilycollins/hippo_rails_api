0b896c1307025a711786a0c64c9d76c20a16bc46
cemilycollins <c.emily.collins@gmail.com>
2019-02-20 16:36:05 -0500

    added model system route and page with signatures

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -11,6 +11,8 @@ class Procedure < ApplicationRecord
   def nat_avg_cost
     @connection = ActiveRecord::Base.connection
     results = @connection.exec_query("SELECT AVG(average_covered_charges) FROM hospital_procedures WHERE procedure_id = #{self.id}")
+    self.nat_avg_cost = results.rows[0][0]
+    self.save
     return results.rows[0][0]
     # arr = []
     # self.hospital_procedures.each do |hp|
@@ -22,12 +24,16 @@ class Procedure < ApplicationRecord
   def total_hospitals
     @connection = ActiveRecord::Base.connection
     results = @connection.exec_query("SELECT COUNT(id) FROM hospital_procedures WHERE procedure_id = #{self.id}")
+    self.total_hospitals = results.rows[0][0]
+    self.save
     return results.rows[0][0]
   end
 
   def total_discharges
     @connection = ActiveRecord::Base.connection
     results = @connection.exec_query("SELECT SUM(total_discharges) FROM hospital_procedures WHERE procedure_id = #{self.id}")
+    self.total_discharges = results.rows[0][0]
+    self.save
     return results.rows[0][0]
   end
 end
